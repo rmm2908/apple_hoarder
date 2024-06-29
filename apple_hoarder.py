7d7536b60b6f75be0fff13dbe46082e25a7fa05d
Ryan Mitwally <ryan.mitwally@edu.fh-joanneum.at>
2024-06-29 11:45:44 +0200

    Put basket class into its own file

--- apple_hoarder.py
+++ apple_hoarder.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from apple_hoarder import Basket
 
 
 class Game:
@@ -110,31 +111,6 @@ class Game:
         print(f'Final Score: {self._score}')
         pygame.quit()
 
-
-class Basket:
-    def __init__(self, screen, sprites: tuple[str]):
-        self._SCREEN = screen
-        self._sprites = sprites
-        self.rect = pygame.image.load(sprites[0]).get_rect()
-        self.rect.x = self._SCREEN.get_width()//2-self.rect.width//2
-        self.rect.y = (self._SCREEN.get_height()//7)*6
-        self._speed = 10
-
-    def move(self, direction):
-        if direction == 'l':
-            self.rect = self.rect.move((-self._speed, 0))
-            if self.rect.x <= 0:
-                self.rect.x = 1
-        if direction == 'r':
-            self.rect = self.rect.move((self._speed, 0))
-            if self.rect.x + self.rect.width >= self._SCREEN.get_width():
-                self.rect.x = self._SCREEN.get_width()-self.rect.width
-
-    def draw(self, frame: int, flip: bool):
-        sprite = pygame.image.load(self._sprites[frame])
-        if flip:
-            sprite = pygame.transform.flip(sprite, True, False)
-        self._SCREEN.blit(sprite, self.rect)
 
 
 class Apple:
---
+++ apple_hoarder/basket.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Basket:
+    def __init__(self, screen, sprites: tuple[str]):
+        self._SCREEN = screen
+        self._sprites = sprites
+        self.rect = pygame.image.load(sprites[0]).get_rect()
+        self.rect.x = self._SCREEN.get_width()//2-self.rect.width//2
+        self.rect.y = (self._SCREEN.get_height()//7)*6
+        self._speed = 10
+
+    def move(self, direction):
+        if direction == 'l':
+            self.rect = self.rect.move((-self._speed, 0))
+            if self.rect.x <= 0:
+                self.rect.x = 1
+        if direction == 'r':
+            self.rect = self.rect.move((self._speed, 0))
+            if self.rect.x + self.rect.width >= self._SCREEN.get_width():
+                self.rect.x = self._SCREEN.get_width()-self.rect.width
+
+    def draw(self, frame: int, flip: bool):
+        sprite = pygame.image.load(self._sprites[frame])
+        if flip:
+            sprite = pygame.transform.flip(sprite, True, False)
+        self._SCREEN.blit(sprite, self.rect)
